3c736dd009c9ee215ace26bbc3fafb8cf99047e5
chcarrie <ppinkywei@gmail.com>
2024-10-28 18:35:05 +0800

    update

--- src/mixins/swal.js
+++ src/mixins/swal.js
@@ -2,7 +2,7 @@ import Swal from 'sweetalert2'
 
 export default function ShowNotification(style, text) {
   Swal.fire({
-    position: 'center-end',
+    position: 'top-end',
     title: text,
     icon: `${style}`,
     timer: 1500,
