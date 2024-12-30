e88d3374f70a5a89d06fe580d0e86ad7370ab9f6
chcarrie <ppinkywei@gmail.com>
2024-12-30 15:09:03 +0800

    update

--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -1,34 +1,34 @@
 <template>
   <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
-      <li class="page-item" :class="{ disabled: !pages.has_pre }">
+      <li class="page-item me-2" :class="{ disabled: !pages.has_pre }">
         <a
-          class="page-link"
+          class="page-link rounded-2"
           href="#"
           aria-label="Previous"
           @click.prevent="updatePage(pages.current_page - 1)"
         >
-          <span aria-hidden="true"><i class="bi bi-caret-left-fill"></i></span>
+          <span aria-hidden="true"><i class="bi bi-chevron-left"></i></span>
         </a>
       </li>
       <li
-        class="page-item"
+        class="page-item me-2"
         v-for="page in pages.total_pages"
         :key="page"
         :class="{ active: page === pages.current_page }"
       >
-        <a class="page-link" href="#" @click.prevent="updatePage(page)">
+        <a class="page-link rounded-2" href="#" @click.prevent="updatePage(page)">
           {{ page }}
         </a>
       </li>
       <li class="page-item" :class="{ disabled: !pages.has_next }">
         <a
-          class="page-link"
+          class="page-link rounded-2"
           href="#"
           aria-label="Next"
           @click.prevent="updatePage(pages.current_page + 1)"
         >
-          <span aria-hidden="true"><i class="bi bi-caret-right-fill"></i></span>
+          <span aria-hidden="true"><i class="bi bi-chevron-right"></i></span>
         </a>
       </li>
     </ul>
